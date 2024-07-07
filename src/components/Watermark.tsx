e20946917e84fa11285c41ebfa606d0dddb243fc
Andry Pebrianto <andrypeb227@gmail.com>
2024-07-07 16:53:47 +0700

    Update copyright (remove dumbways part)

--- src/components/Watermark.tsx
+++ src/components/Watermark.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardBody, Image, Text } from "@chakra-ui/react";
+import { Box, Card, CardBody, Text } from "@chakra-ui/react";
 import { Fragment } from "react";
 import {
   FaGithub,
@@ -13,7 +13,7 @@ export default function Watermark() {
       <Card bg={"#3a3a3a"} color={"white"}>
         <CardBody py={4} px={5}>
           <Box fontSize={"md"}>
-            Developed by Andry Pebrianto - {""}
+            Created By Andry Pebrianto - {""}
             <FaGithub style={{ display: "inline", marginRight: "5px" }} />
             <FaLinkedin style={{ display: "inline", marginRight: "5px" }} />
             <FaSquareFacebook
@@ -23,17 +23,8 @@ export default function Watermark() {
               style={{ display: "inline", marginRight: "5px" }}
             />
           </Box>
-          <Text fontSize={"xs"} color={"gray.400"}>
-            Powered by{" "}
-            <Image
-              src="https://raw.githubusercontent.com/taufik-hdyt/dwonsitecircle/master/src/assets/logo.png"
-              alt="Dumbways Logo"
-              width={"20px"}
-              display={"inline"}
-              position={"relative"}
-              bottom={"-3px"}
-            />{" "}
-            Dumbways Indonesia - #1 Coding Bootcamp
+          <Text fontSize={"xs"} marginTop={"20px"} textAlign={"end"} color={"gray.400"}>
+            Copyright &copy; ${new Date().getFullYear()}
           </Text>
         </CardBody>
       </Card>
